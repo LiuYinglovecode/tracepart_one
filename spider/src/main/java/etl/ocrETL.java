dfc4b40576f3be3d54c253ff5d1b465387a9b0ce
liyujie <1987403512@qq.com>
2019-04-24 09:41:47 +0800

    ocr文本解析

--- spider/src/main/java/etl/ocrETL.java
+++ spider/src/main/java/etl/ocrETL.java
@@ -7,18 +7,26 @@ import util.HttpRequestUtils;
 
 public class ocrETL {
     private final static Logger LOGGER = LoggerFactory.getLogger(ocrETL.class);
+    private static String weifuwuPDF = "http://172.17.20.119:8081/1/ocr/pdf?pdf=";
+    private static String weifuwuImage = "http://172.17.20.119:8081/1/ocr/baiduImage?image=";
 
     public static void main(String[] args) {
+        ocrETL ocr = new ocrETL();
+        ocr.getInfoFromPDF("C:/Users/liyujie/Desktop/neimeng/image.pdf");
+    }
+
+    public void getInfoFromPDF(String pdfPath) {
         try {
-            String result = HttpRequestUtils.httpPost("http://172.17.20.119:8081/1/ocr/pdf?pdf=F:/5.pdf", "");
+            String result = HttpRequestUtils.httpPost(weifuwuPDF + pdfPath, "");
             if (null != result) {
-                JSONObject jsonObject = new JSONObject(Boolean.parseBoolean(result));
+                JSONObject jsonObject = (JSONObject) JSONObject.parse(result);
+                String info = jsonObject.getString("res");
+                String[] list = info.split("\\n");
             } else {
                 LOGGER.info("http post result null");
             }
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
         }
-
     }
 }
