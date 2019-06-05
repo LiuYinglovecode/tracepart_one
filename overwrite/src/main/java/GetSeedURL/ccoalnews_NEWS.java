6cca093062feed72d4a7a9b4855d5d2b7969692e
liyujie <1987403512@qq.com>
2019-06-05 12:06:23 +0800

    添加依赖

---
+++ overwrite/src/main/java/GetSeedURL/ccoalnews_NEWS.java
@@ -0,0 +1,10 @@
+package GetSeedURL;
+
+import news.parse.cinn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ccoalnews_NEWS {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ccoalnews_NEWS.class);
+
+}
