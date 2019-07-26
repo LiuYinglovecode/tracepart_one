085d92356e6889dfa4f230bd7ca93e89e0b35653
chenyan <cyan_1123@163.com>
2019-07-26 11:41:56 +0800

    代码更新

---
+++ overwrite/src/main/java/mainEntryCompany/DownloadMainEntry.java
@@ -0,0 +1,69 @@
+package mainEntryCompany;
+
+import Utils.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import parse.company.download.Net114Download;
+import parse.company.download.QiyiDownload;
+import parse.company.download.YellowurlDownload;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+public class DownloadMainEntry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DownloadMainEntry.class);
+
+    public static void main(String[] args) {
+        DownloadMainEntry downloadMainEntry = new DownloadMainEntry();
+        downloadMainEntry.getFromRedis();
+    }
+
+    private void getFromRedis() {
+        try {
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 120, TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<Runnable>(15), new ThreadPoolExecutor.CallerRunsPolicy());
+            String url = "";
+            while (true) {
+                while (null != (url = RedisUtil.getUrlFromeSet("toCatchUrl-Company"))) {
+                    if (!RedisUtil.isExist("catchedUrl-Company", url)) {
+                        SeedTask seed = new SeedTask(url);
+                        executor.execute(seed);
+                    }
+                }
+                executor.shutdown();
+                Thread.sleep(60000);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private class SeedTask implements Runnable {
+        private String taskName;
+
+        private SeedTask(String name) {
+            this.taskName = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (taskName.contains("71.net")) {
+                    QiyiDownload qiyiDownload = new QiyiDownload();
+                    qiyiDownload.companyInfo(taskName);
+                }else if (taskName.contains("corp.net114.com")) {
+                    Net114Download net114Download = new Net114Download();
+                    net114Download.companyInfo(taskName);
+                }else if (taskName.contains("yellowurl")) {
+                    YellowurlDownload yellowurlDownload = new YellowurlDownload();
+                    yellowurlDownload.info(taskName);
+                }
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage());
+            }
+            LOGGER.info(taskName + " : 执行完毕");
+        }
+    }
+}
---
+++ overwrite/src/main/java/mainEntryCompany/SeedMainEntry.java
@@ -0,0 +1,74 @@
+package mainEntryCompany;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import parse.company.toRedis.Net114ToRedis;
+import parse.company.toRedis.QiyiToRedis;
+import parse.company.toRedis.YellowurlToRedis;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lyj
+ */
+public class SeedMainEntry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeedMainEntry.class);
+
+    public static void main(String[] args) {
+        try {
+            SeedMainEntry mainEntry = new SeedMainEntry();
+            mainEntry.SeedUrlList();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private void SeedUrlList() {
+        try {
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 120, TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<Runnable>(15), new ThreadPoolExecutor.CallerRunsPolicy());
+            InputStream path = this.getClass().getResourceAsStream("/CompanySeedUrl.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(path));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                SeedTask seed = new SeedTask(line);
+                executor.execute(seed);
+            }
+            executor.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SeedTask implements Runnable {
+        private String taskName;
+
+        private SeedTask(String name) {
+            this.taskName = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (taskName.contains("71.net")) {
+                    QiyiToRedis qiyiToRedis = new QiyiToRedis();
+                    qiyiToRedis.homepage(taskName);
+                }else if (taskName.contains("corp.net114.com")) {
+                    Net114ToRedis net114ToRedis = new Net114ToRedis();
+                    net114ToRedis.homepage(taskName);
+                }else if (taskName.contains("yellowurl")) {
+                    YellowurlToRedis yellowurlToRedis = new YellowurlToRedis();
+                    yellowurlToRedis.category(taskName);
+                }
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage());
+            }
+            LOGGER.info(taskName + " : 执行完毕");
+        }
+    }
+}
